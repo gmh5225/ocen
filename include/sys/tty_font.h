c3e5541db4be1ed3a45d82c989327636e18ea8b4
pizcan <140350299+pisscan@users.noreply.github.com>
2023-10-07 21:55:42 -0700

    Update tty_font.h

--- include/sys/tty_font.h
+++ include/sys/tty_font.h
@@ -1,1 +1,11 @@
+#ifndef _LIB_TTY_FONT_H_
+#define _LIB_TTY_FONT_H_
 
+#include <sys/types.h>
+
+#define FONT_WIDTH 8
+#define FONT_HEIGHT 16
+
+extern const uint8_t DEFAULT_FONT_DATA[];
+
+#endif  /* !_LIB_TTY_FONT_H_ */
