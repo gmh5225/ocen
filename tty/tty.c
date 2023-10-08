5734102f07af7ad743ca83ce02f9cb11e3164802
pisscan <epicgamirr133769@gmail.com>
2023-10-08 01:04:06 +0000

    Added VGA Frame Buffer

--- tty/tty.c
+++ tty/tty.c
@@ -1,1 +1,18 @@
+#include <gdt.h>
+#include <root.h>
+#include <idt.h>
+#include <rendr/vga>
 
+struct vga_initialize {
+	void set_background_color(uint8_t color) {
+		for(int i = 0; i < 80 * 25; ++i) {
+			vga_buffer[i] = (vga_buffer[i] & 0xFF | (color << 8);
+
+		}
+	}
+}
+
+void bc() {
+	vga_initialize run;
+	run.set_background_color(VGA_BLACK);
+}
