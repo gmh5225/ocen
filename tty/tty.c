704065608d09d894302c029e436a95f53d3c90b9
pizcan <140350299+pisscan@users.noreply.github.com>
2023-10-08 13:44:13 -0700

    Cursor Blinking

--- tty/tty.c
+++ tty/tty.c
@@ -1,13 +1,30 @@
-#include <sys/tty.h>
-#include <sys/cdefs.h>
-#include <sys/errno.h>
-#include <sys/ascii.h>
-#include <string.h>
-#include <sys/tty_font.h>
+#include <tty/tty.h>
 
-#include "tty/fetch.h"
+/*
+ * Default cursor color.
+ *
+ * TODO: The cursor color should
+ *       be the invert of the color
+ *       of whatever it is on top of.
+ */
 
 #define DEFAULT_CURSOR_BG 0x808080
 
-#define CURSOR_WIDTH            FONT_WIDTH
-#define CURSOR_HEIGHT           FONT_HEIGHT
+static void tty_draw_cursor(struct tty *tty, bool hide)
+{
+    uint32_t x, y, idx;
+    uint32_t *fb_ptr;
+    uint32_t color;
+
+    fb_ptr = tty->fbdev.mem;
+    color = hide ? tty->bg : DEFAULT_CURSOR_BG;
+
+    for (size_t cy = 0; cy < CURSOR_HEIGHT; ++cy) {
+        for (size_t cx = 0; cx < CURSOR_WIDTH; ++cx) {
+            x = tty->curspos_x + cx;
+            y = tty->curspos_y + cy;
+            idx = fbdev_get_index(&tty->fbdev, x, y);
+            fb_ptr[idx] = color;
+        }
+    }
+}
